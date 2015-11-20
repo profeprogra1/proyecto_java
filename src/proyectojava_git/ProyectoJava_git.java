f5756b9a0165e666fb961c49f0032f3c85b72f1a
ricardo <ricardo@mbp-de-ricardo>
2015-11-20 15:58:02 -0300

    clase Conexion , 4

--- src/proyectojava_git/ProyectoJava_git.java
+++ src/proyectojava_git/ProyectoJava_git.java
@@ -1,5 +1,6 @@
 package proyectojava_git;
 
+import com.proyectojava.conexion.Conexion;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.Statement;
@@ -8,24 +9,8 @@ import java.util.Scanner;
 public class ProyectoJava_git {
 
     public static void main(String[] args) {
-        String usuario = "root";
-        String clave = "123456";
-        Scanner in = new Scanner(System.in);
-        try {
-            Class.forName("org.gjt.mm.mysql.Driver");
-            String url = "jdbc:mysql://localhost:3306/test01";
-            Connection con = DriverManager.getConnection(url, usuario, clave);
-            Statement stmt = con.createStatement();
-            //INSERTAR
-            stmt.executeUpdate("insert into Usuarios(nombre,apellido) values('A','B')");
-            //ELIMINAR
-            stmt.executeUpdate("delete from Usuarios where usuario_id=1");
-            //ACTUALIZAR
-            stmt.executeUpdate("update Usuarios set nombre='ALGO' where usuario_id=1");
-
-        } catch (Exception ex) {
-            System.out.println("ERROR:" + ex.getMessage());
-        }
+        Conexion con=new Conexion();
+        con.ejecutarConsulta("insert into Usuarios(nombre,apellido) values('GIT','HUB')");
     }
 
 }

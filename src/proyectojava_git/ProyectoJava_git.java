e977caa150c9331dd5d622e7c9bf306924105043
ricardo <ricardo@mbp-de-ricardo>
2015-11-20 15:36:18 -0300

    4.0

--- src/proyectojava_git/ProyectoJava_git.java
+++ src/proyectojava_git/ProyectoJava_git.java
@@ -1,22 +1,31 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package proyectojava_git;
 
-/**
- *
- * @author ricardo
- */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+import java.util.Scanner;
+
 public class ProyectoJava_git {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        System.out.println("Primer proyecto en git...");
-        System.out.println("SOY ALUMNO MODIFICANDO PROYECTO!!!");
+        String usuario = "root";
+        String clave = "123456";
+        Scanner in = new Scanner(System.in);
+        try {
+            Class.forName("org.gjt.mm.mysql.Driver");
+            String url = "jdbc:mysql://localhost:3306/test01";
+            Connection con = DriverManager.getConnection(url, usuario, clave);
+            Statement stmt = con.createStatement();
+            //INSERTAR
+            stmt.executeUpdate("insert into Usuarios(nombre,apellido) values('A','B')");
+            //ELIMINAR
+            stmt.executeUpdate("delete from Usuarios where usuario_id=1");
+            //ACTUALIZAR
+            stmt.executeUpdate("update Usuarios set nombre='ALGO' where usuario_id=1");
+
+        } catch (Exception ex) {
+            System.out.println("ERROR:" + ex.getMessage());
+        }
     }
-    
+
 }

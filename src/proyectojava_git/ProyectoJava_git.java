8482c7037f119fc031d18d2a7e34ab3ed3b9826b
ricardo <ricardo@mbp-de-ricardo>
2015-11-20 12:13:32 -0300

    Mi primer proyecto en git, 2

--- src/proyectojava_git/ProyectoJava_git.java
+++ src/proyectojava_git/ProyectoJava_git.java
@@ -15,7 +15,7 @@ public class ProyectoJava_git {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Primer proyecto en git...");
     }
     
 }

23d15a79d0b04d636da9664fed1ccc1b6ef9573e
ricardo <ricardo@10.34.12.254>
2015-11-24 19:16:03 -0300

    24/11/2015 19:15

---
+++ src/com/ricardo/negocio/Usuario.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ricardo.negocio;
+
+import com.ricardo.Conexion.Conexion;
+
+/**
+ *
+ * @author ricardo
+ */
+public class Usuario {
+    private int usuario_id;
+    private String nombre;
+    private String apellido;
+    Conexion con;
+    public Usuario() {
+        con=new Conexion();
+    }
+
+    public int getUsuario_id() {
+        return usuario_id;
+    }
+
+    public void setUsuario_id(int usuario_id) {
+        this.usuario_id = usuario_id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+    public void save(){
+        con.setEjecutar("insert into usuarios(nombre,apellido) values('LA'.'LE')");
+    }
+}
--- src/proyectojava_git/ProyectoJava_git.java
+++ src/proyectojava_git/ProyectoJava_git.java
@@ -1,17 +1,12 @@
 package proyectojava_git;
 
-import com.ricardo.Conexion.Conexion;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import com.ricardo.negocio.Usuario;
 
 public class ProyectoJava_git {
 
     public static void main(String[] args) {
-        Conexion con=new Conexion();
-        con.setEjecutar("insert into usuarios(nombre,apellido) values('LA','PO')");
-
+        Usuario usuario=new Usuario();
+        usuario.save();
     }
 
 }

45ef056b0bc37e5ccc319e7d7565674a9f95eea4
ricardo <ricardo@10.34.12.254>
2015-11-24 19:07:32 -0300

    24/11/2015 19:07

---
+++ src/com/ricardo/Conexion/Conexion.java
@@ -0,0 +1,35 @@
+package com.ricardo.Conexion;
+//CAPA ACCESO A DATOS
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class Conexion {
+
+    Connection con;
+    Statement stmt;
+    ResultSet rs;
+    String usuario = "root";
+    String clave = "123456";
+    String url = "jdbc:mysql://localhost:3306/test01";
+    public Conexion() {
+        try {
+            Class.forName("org.gjt.mm.mysql.Driver");
+            con = DriverManager.getConnection(url, usuario, clave);
+        } catch (Exception ex) {
+            System.out.println("ERROR:" + ex.getMessage());
+        }
+    }
+    public void setEjecutar(String sql){
+        try{
+            stmt=con.createStatement();
+            stmt.executeUpdate(sql);
+        }catch(Exception ex){
+            System.out.println("EJECUTAR ERROR:"+ex.getMessage());
+        }
+    }
+    
+
+}
--- src/proyectojava_git/ProyectoJava_git.java
+++ src/proyectojava_git/ProyectoJava_git.java
@@ -1,5 +1,6 @@
 package proyectojava_git;
 
+import com.ricardo.Conexion.Conexion;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -8,19 +9,8 @@ import java.sql.Statement;
 public class ProyectoJava_git {
 
     public static void main(String[] args) {
-        try {
-            Connection con;
-            Statement stmt;
-            ResultSet rs;
-            String usuario = "root";
-            String clave = "123456";
-
-            Class.forName("org.gjt.mm.mysql.Driver");
-            String url = "jdbc:mysql://localhost:3306/test01";
-            con = DriverManager.getConnection(url, usuario, clave);
-        } catch (Exception ex) {
-            System.out.println("ERROR:" + ex.getMessage());
-        }
+        Conexion con=new Conexion();
+        con.setEjecutar("insert into usuarios(nombre,apellido) values('LA','PO')");
 
     }
 

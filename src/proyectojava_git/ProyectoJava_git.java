d234b10d7a76b49dba79d4a1cfe9b1e823695db6
ricardo <ricardo@10.34.12.254>
2015-11-24 19:26:49 -0300

    24/11/2015 19:26

--- src/proyectojava_git/ProyectoJava_git.java
+++ src/proyectojava_git/ProyectoJava_git.java
@@ -1,11 +1,19 @@
 package proyectojava_git;
 
 import com.ricardo.negocio.Usuario;
+import java.util.Scanner;
 
 public class ProyectoJava_git {
 
     public static void main(String[] args) {
         Usuario usuario=new Usuario();
+        Scanner in=new Scanner(System.in);
+        System.out.println("NOMBRE;");
+        String nom=in.nextLine();
+        usuario.setNombre(nom);
+        System.out.println("APE:");
+        String ape=in.nextLine();
+        usuario.setApellido(ape);
         usuario.save();
     }
 

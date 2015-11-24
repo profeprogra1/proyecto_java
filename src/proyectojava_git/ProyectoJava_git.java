1c0ff796c652433d85af0a1351dbed5cf71dafaf
ricardo <ricardo@10.34.12.254>
2015-11-24 18:55:18 -0300

    24/11/2015 18:50

--- src/proyectojava_git/ProyectoJava_git.java
+++ src/proyectojava_git/ProyectoJava_git.java
@@ -1,12 +1,27 @@
 package proyectojava_git;
 
-import com.proyectojava.conexion.Conexion;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 public class ProyectoJava_git {
 
     public static void main(String[] args) {
-        Conexion con=new Conexion();
-        con.ejecutarConsulta("insert into Usuarios(nombre,apellido) values('GIT','HUB')");
+        try {
+            Connection con;
+            Statement stmt;
+            ResultSet rs;
+            String usuario = "root";
+            String clave = "123456";
+
+            Class.forName("org.gjt.mm.mysql.Driver");
+            String url = "jdbc:mysql://localhost:3306/test01";
+            con = DriverManager.getConnection(url, usuario, clave);
+        } catch (Exception ex) {
+            System.out.println("ERROR:" + ex.getMessage());
+        }
+
     }
 
 }
